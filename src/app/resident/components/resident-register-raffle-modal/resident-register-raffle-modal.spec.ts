ee48385468fe0f0c7dbdf0d0dbe0c86581b0a304
cbcristhian <cristhian.castro@unosquare.com>
2025-12-13 19:12:19 -0400

    Unit testing for raffle registration

--- src/app/resident/components/resident-register-raffle-modal/resident-register-raffle-modal.spec.ts
+++ src/app/resident/components/resident-register-raffle-modal/resident-register-raffle-modal.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ResidentRegisterRaffleModal } from './resident-register-raffle-modal';
+import { ResidentService } from '../../services/resident-service';
+import { of } from 'rxjs';
 
 describe('ResidentRegisterRaffleModal', () => {
   let component: ResidentRegisterRaffleModal;
   let fixture: ComponentFixture<ResidentRegisterRaffleModal>;
 
+  const mockResidentService = {
+    registerForRaffle: jasmine
+      .createSpy('registerForRaffle')
+      .and.returnValue(of('Registered successfully')),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ResidentRegisterRaffleModal],
+      providers: [{ provide: ResidentService, useValue: mockResidentService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ResidentRegisterRaffleModal);
@@ -18,5 +26,14 @@ describe('ResidentRegisterRaffleModal', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call registerForRaffle and emit registered event', () => {
+    const emitSpy = spyOn(component.registered, 'emit');
+
+    component.register();
+
+    expect(mockResidentService.registerForRaffle).toHaveBeenCalled();
+    expect(emitSpy).toHaveBeenCalledWith('Registered successfully');
   });
 });
